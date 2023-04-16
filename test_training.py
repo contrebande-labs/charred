a09ecb7de4b8b028507b3677c01223dc18783b5b
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-15 20:58:18 -0400

    found a diffusion model with flax checkpoint for testing.

--- test_training.py
+++ test_training.py
@@ -7,7 +7,8 @@ def main():
     tokenizer, text_encoder, vae, vae_params, unet, unet_params = setup_model(
         seed=None, mixed_precision=None,
         pretrained_text_encoder_model_name_or_path="google/byt5-base", pretrained_text_encoder_model_revision=None, 
-        pretrained_diffusion_model_name_or_path="stabilityai/stable-diffusion-2-1", pretrained_diffusion_model_revision=None)
+        # TODO: change for stabilityai/stable-diffusion-2-1" vae and unet converted to flax msgpack format
+        pretrained_diffusion_model_name_or_path="glenn2/diffusion-2-flax", pretrained_diffusion_model_revision=None)
 
 if __name__ == "__main__":
     main()
