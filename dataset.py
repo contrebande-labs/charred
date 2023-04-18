b09bb7b2845a96300545acd5d2299ba447066a93
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-18 10:51:10 -0400

    dataset improvements

--- dataset.py
+++ dataset.py
@@ -1,31 +1,80 @@
 import os
 
 from datasets import load_dataset, load_from_disk
-from dataset_preprocessing import dataset_transforms
-from dataset_filtering import dataset_filter
+from torchvision import transforms
+from PIL import Image
+import requests
 
 DATASET_OUTPUT_DIR = "/data/dataset/charred"
+SAVE_DATASET_TO_DISK = False
+
+def _dataset_transforms(tokenizer, image_transforms, example):
+
+    caption = example['caption']
+    image_url = example['url']
+    watermark_probability = example['pwatermark']
+  
+    filter_pass = caption is not None and isinstance(caption, str) and image_url is not None and isinstance(image_url, str) and watermark_probability < 0.6
+
+    example["pass"] = filter_pass
+
+    if filter_pass:
+
+        image_bytes = requests.get(image_url, stream = True).raw
+
+        # TODO: if checksum fails, skip this entry and filter out later 
+        #checksum = hashlib.md5(image_bytes).hexdigest() == example["hash"]
+
+        # append image data
+        # TODO: apply and cache image embbedings here instead of in the training loop (and don't keep the pixel data)
+        example["pixel_values"] = image_transforms(Image.open(image_bytes).convert("RGB"))
+
+        # append tokenized text
+        # TODO: apply and cache text embbedings here instead of in the training loop (and don't keep the tokenized text)
+        example["input_ids"] = tokenizer(caption, max_length=tokenizer.model_max_length, padding="do_not_pad", truncation=True).input_ids
+ 
+    return example
+
+def dataset_transforms(tokenizer, resolution):
+
+    # TODO: replace with https://jax.readthedocs.io/en/latest/jax.image.html
+    image_transforms = transforms.Compose(
+        [
+            transforms.Resize(resolution, interpolation=transforms.InterpolationMode.LANCZOS),
+            transforms.RandomCrop(resolution),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]),
+        ]
+    )
+ 
+    return lambda example: _dataset_transforms(tokenizer, image_transforms, example)
 
 def setup_dataset(cache_dir, resolution, tokenizer):
 
     # TODO: make sure we use the datatsets library with JAX : https://huggingface.co/docs/datasets/use_with_jax
     # loading the dataset
 
-    if os.path.isdir(DATASET_OUTPUT_DIR):
-        load_from_disk = load_from_disk(DATASET_OUTPUT_DIR)
+    if os.path.isdir(DATASET_OUTPUT_DIR) and SAVE_DATASET_TO_DISK:
+        dataset = load_from_disk(DATASET_OUTPUT_DIR)
     else:
         dataset = load_dataset(
-            "laion/laion-high-resolution",
+            path="laion/laion-high-resolution",
             cache_dir=os.path.join(cache_dir, "laion-high-resolution"),
             split='train',
             streaming=True
-        ).filter(
-            dataset_filter()
+        ).shuffle(
+            seed=27,
+            buffer_size=10_000
+        ).take(
+            n=1_000_000 # take 1M out of 170M
         ).map(
             transforms=dataset_transforms(tokenizer, resolution),
             remove_columns=[],
-            batched=False
+            batched=False #TODO: maybe batch this?
+        ).filter(
+            filter=lambda example: example["pass"]
         )
-        dataset.save_to_disk(DATASET_OUTPUT_DIR)
+        if SAVE_DATASET_TO_DISK:
+            dataset.save_to_disk(DATASET_OUTPUT_DIR)
 
     return dataset
--- dataset_filtering.py
+++
@@ -1,9 +0,0 @@
-def _dataset_filter(example):
-  caption = example['caption']
-  image_url = example['url']
-  watermark_probability = example['pwatermark']
-  return caption is not None and isinstance(caption, str) and image_url is not None and isinstance(image_url, str) and watermark_probability < 0.6
-
-def dataset_filter():
-  # TODO: drop duplicates with image embeddings https://arxiv.org/abs/2303.12733 https://github.com/LAION-AI/laion-dedup https://github.com/LAION-AI/image-deduplication-testset
-  return lambda example: _dataset_filter(example)
