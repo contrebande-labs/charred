36203ef42c95cd1300a1d6be9b3f955f407a6280
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-18 00:02:10 -0400

    debugging interleave_datasets

--- dataset.py
+++ dataset.py
@@ -14,13 +14,13 @@ def setup_dataset(cache_dir, image_column, caption_column, resolution, tokenizer
     # TODO: drop images that are smaller than 1024x1024 https://huggingface.co/docs/datasets/stream#filter https://huggingface.co/docs/datasets/process#select-and-filter
     # TODO: drop images with watermarks (joined) https://laion.ai/blog/laion-5b/
     # TODO: drop images columns https://huggingface.co/docs/datasets/stream#rename-remove-and-cast
-    laion2b_en = load_dataset("laion/laion2b-en-vit-l-14-embeddings", cache_dir=os.path.join(cache_dir, "laion2B-en"), streaming=True).shuffle(seed=0, buffer_size=10_000)
-    laion2b_multi = load_dataset("laion/laion2b-multi-vit-l-14-embeddings", cache_dir=os.path.join(cache_dir, "laion2B-multi"), streaming=True).shuffle(seed=1, buffer_size=10_000)
-    laion2b_nolang = load_dataset("laion/laion1b-nolang-vit-l-14-embeddings", cache_dir=os.path.join(cache_dir, "laion1B-nolang"), streaming=True).shuffle(seed=2, buffer_size=10_000)
+    laion2b_en = load_dataset("laion/laion2b-en-vit-l-14-embeddings", cache_dir=os.path.join(cache_dir, "laion2B-en"), streaming=True)
+    laion2b_multi = load_dataset("laion/laion2b-multi-vit-l-14-embeddings", cache_dir=os.path.join(cache_dir, "laion2B-multi"), streaming=True)
+    laion2b_nolang = load_dataset("laion/laion1b-nolang-vit-l-14-embeddings", cache_dir=os.path.join(cache_dir, "laion1B-nolang"), streaming=True)
 
     # concatenate the datasets
     # TODO: add probabilities https://huggingface.co/docs/datasets/process#interleave https://huggingface.co/docs/datasets/stream#interleave
-    dataset = interleave_datasets([laion2b_en, laion2b_multi, laion2b_nolang])["train"]
+    dataset = interleave_datasets([laion2b_en, laion2b_multi, laion2b_nolang])
 
     # setting up the transform
     dataset.set_transform(dataset_transform(image_column, caption_column, tokenizer, resolution))
