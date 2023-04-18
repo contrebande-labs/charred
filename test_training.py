215e843e7044cdbdcc6c2eef3dfe7a75ec27f169
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-17 21:13:42 -0400

    fixing dataset setup arguments

--- test_training.py
+++ test_training.py
@@ -11,7 +11,7 @@ def main():
         # TODO: change for stabilityai/stable-diffusion-2-1" vae and unet converted to flax msgpack format?
         pretrained_diffusion_model_name_or_path="flax/stable-diffusion-2-1", pretrained_diffusion_model_revision=None)
 
-    setup_dataset("/data/dataset/cache", "image", "text", 1024, False, False, tokenizer)
+    setup_dataset("/data/dataset/cache", "image", "text", 1024, tokenizer)
 
 if __name__ == "__main__":
     main()
