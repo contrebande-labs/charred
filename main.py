359aaf9c167c37b55a875f147e98a8bad5f70951
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-18 22:27:07 -0400

    wandb improvements

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ def main():
     # Setup WandB for logging & tracking
     log_wandb = args.log_wandb
     if log_wandb:
-        wandb.define_metric("*", step_metric="train/step")
-        wandb.define_metric("train/step", step_metric="walltime")
         wandb.init(
             entity="charred",
             project="charred",
@@ -34,6 +32,8 @@ def main():
         wandb.config.update({
             "num_devices": jax.device_count(),
         })
+        wandb.define_metric("*", step_metric="train/step")
+        wandb.define_metric("train/step", step_metric="walltime")
 
     # init random number generator
     seed = args.seed
