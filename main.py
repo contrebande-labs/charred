00d9547c4e69bdbb8a697525de7d7be21e8a6e79
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-19 11:37:20 -0400

    unet param init readability

--- main.py
+++ main.py
@@ -35,12 +35,12 @@ def main():
         wandb.define_metric("*", step_metric="train/step")
         wandb.define_metric("train/step", step_metric="walltime")
 
+    repo_id = create_repository(args.output_dir, args.push_to_hub, args.hub_model_id, args.hub_token)
+
     # init random number generator
     seed = args.seed
     seed_rng = jax.random.PRNGKey(seed)
     rng, rng_params = jax.random.split(seed_rng)
-
-    repo_id = create_repository(args.output_dir, args.push_to_hub, args.hub_model_id, args.hub_token)
 
     # Pretrained freezed model setup
     tokenizer, text_encoder, vae, vae_params, unet = setup_model(
@@ -52,9 +52,11 @@ def main():
     optimizer = setup_optimizer(args.learning_rate, args.adam_beta1, args.adam_beta2, args.adam_epsilon, args.adam_weight_decay, args.max_grad_norm)
 
     # State setup
-    # TODO: find out why we are passing params=unet_params. Only for shape?
-    unet_params = unfreeze(unet.init_weights(rng=rng_params))
-    replicated_state = jax_utils.replicate(train_state.TrainState.create(apply_fn=unet.__call__, params=unet_params, tx=optimizer))
+    replicated_state = jax_utils.replicate(train_state.TrainState.create(
+        apply_fn=unet.__call__,
+        params=unfreeze(unet.init_weights(rng=rng_params)),
+        tx=optimizer
+    ))
     replicated_text_encoder_params = jax_utils.replicate(text_encoder.params)
     replicated_vae_params = jax_utils.replicate(vae_params)
 
