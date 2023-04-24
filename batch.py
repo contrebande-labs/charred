ff9fb328722b47902a52f7f6c7d6ed5a1dcc4fd9
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-24 11:18:13 -0400

    dataset preprocessing fixes

--- batch.py
+++ batch.py
@@ -1,31 +1,29 @@
 import torch
 
+def setup_dataloader(dataset, batch_size):
 
-def collate(samples):
+    def _collate(samples):
 
-    # TODO: replace torch.stack with https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.stack.html
+        # TODO: replace torch.stack with https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.stack.html
 
-    pixel_values = (
-        torch.stack([sample["pixel_values"] for sample in samples])
-        .to(memory_format=torch.contiguous_format)
-        .float()
-        .numpy()
-    )
+        pixel_values = (
+            torch.stack([sample["pixel_values"] for sample in samples])
+            .to(memory_format=torch.contiguous_format)
+            .float()
+            .numpy()
+        )
 
-    input_ids = torch.stack([sample["input_ids"] for sample in samples]).numpy()
+        input_ids = torch.stack([sample["input_ids"] for sample in samples]).numpy()
 
-    return {
-        "pixel_values": pixel_values,
-        "input_ids": input_ids,
-    }
-
-
-def setup_dataloader(train_dataset, train_batch_size):
+        return {
+            "pixel_values": pixel_values,
+            "input_ids": input_ids,
+        }
 
     return torch.utils.data.DataLoader(
         # we don't shuffle here because the dataset is already shuffled
-        train_dataset,
-        collate_fn=collate,
-        batch_size=train_batch_size,
+        dataset,
+        collate_fn=_collate,
+        batch_size=batch_size,
         drop_last=True,
     )
