14c911085fdd12af295ed0c135d7bcf1418b881d
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-24 20:37:55 -0400

    tweaked training steps

--- training.sh
+++ training.sh
@@ -1,3 +1,3 @@
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=34359738368
 #export JAX_PLATFORMS=''
-python3 main.py --push_to_hub True --max_train_steps 10_000 --num_train_epochs 1_000_000
+python3 main.py --push_to_hub True --max_train_steps 9_985 --num_train_epochs 1_000_000
