2039162998d5056db9088b14a3547270c9f2dd84
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-04-26 16:25:55 -0400

    validation fixes

--- validation.py
+++ validation.py
@@ -1,5 +1,5 @@
 import jax
-from logging import Image, wandb_log_validation
+from logging import wandb_log_validation
 from flax.training.common_utils import shard
 
 from diffusers import (
@@ -9,18 +9,19 @@ from diffusers import (
     FlaxUNet2DConditionModel,
 )
 
-from transformers import AutoTokenizer, FlaxT5Model
+from transformers import ByT5Tokenizer, FlaxT5Model
 
+def get_validate_lambda(pretrained_unet_path, pipeline_params, rng):
 
-def validate():
-
-    # pipeline_params = pipeline_params.copy()
-    # pipeline_params["controlnet"] = controlnet_params
+    tokenizer = ByT5Tokenizer()
+ 
+    text_encoder = FlaxT5Model.from_pretrained("google/byt5-base")
 
     vae = FlaxAutoencoderKL.from_pretrained(
         "flax/stable-diffusion-2-1", subfolder="vae"
     )
-    unet = FlaxUNet2DConditionModel.from_pretrained("character-aware-diffusion/charred")
+
+    unet = FlaxUNet2DConditionModel.from_pretrained(pretrained_unet_path)
 
     scheduler = FlaxDDPMScheduler(
         beta_start=0.00085,
@@ -29,11 +30,8 @@ def validate():
         num_train_timesteps=1000,
     )
 
-    tokenizer = AutoTokenizer.from_pretrained("google/byt5-base")
-    lm = FlaxT5Model.from_pretrained("google/byt5-base")
-
     pipeline = FlaxStableDiffusionPipeline(
-        text_encoder=lm.encode,
+        text_encoder=text_encoder.encode,
         vae=vae,
         unet=unet,
         tokenizer=tokenizer,
@@ -43,58 +41,52 @@ def validate():
     )
 
     num_samples = jax.device_count()
-    prng_seed = jax.random.split(rng, jax.device_count())
 
-    if len(args.validation_image) == len(args.validation_prompt):
-        validation_images = args.validation_image
-        validation_prompts = args.validation_prompt
-    elif len(args.validation_image) == 1:
-        validation_images = args.validation_image * len(args.validation_prompt)
-        validation_prompts = args.validation_prompt
-    elif len(args.validation_prompt) == 1:
-        validation_images = args.validation_image
-        validation_prompts = args.validation_prompt * len(args.validation_image)
-    else:
-        raise ValueError(
-            "number of `args.validation_image` and `args.validation_prompt` should be checked in `parse_args`"
-        )
+    prng_seed = jax.random.split(rng, num_samples)
 
-    image_logs = []
+    def __validate_lambda(validation_prompts, validation_images):
 
-    for validation_prompt, validation_image in zip(
-        validation_prompts, validation_images
-    ):
-        prompts = num_samples * [validation_prompt]
-        prompt_ids = pipeline.prepare_text_inputs(prompts)
-        prompt_ids = shard(prompt_ids)
+        image_logs = []
 
-        validation_image = Image.open(validation_image).convert("RGB")
-        processed_image = pipeline.prepare_image_inputs(
-            num_samples * [validation_image]
-        )
-        processed_image = shard(processed_image)
-        images = pipeline(
-            prompt_ids=prompt_ids,
-            image=processed_image,
-            params=pipeline_params,
-            prng_seed=prng_seed,
-            num_inference_steps=50,
-            jit=True,
-        ).images
+        for validation_prompt, validation_image in zip(
+            validation_prompts, validation_images
+        ):
 
-        images = images.reshape(
-            (images.shape[0] * images.shape[1],) + images.shape[-3:]
-        )
-        images = pipeline.numpy_to_pil(images)
+            text_inputs = shard(
+                pipeline.prepare_text_inputs(
+                    num_samples * [validation_prompt]
+                )
+            )
 
-        image_logs.append(
-            {
-                "validation_image": validation_image,
-                "images": images,
-                "validation_prompt": validation_prompt,
-            }
-        )
+            image_inputs = shard(
+                pipeline.prepare_image_inputs(
+                    num_samples * [validation_image]
+                )
+            )
 
-    wandb_log_validation(image_logs)
+            output_images = pipeline(
+                prompt_ids=text_inputs,
+                image=image_inputs,
+                params=pipeline_params,
+                prng_seed=prng_seed,
+                num_inference_steps=50,
+                jit=True,
+            ).images
 
-    return image_logs
+            reshaped_output_pil_images = pipeline.numpy_to_pil(
+                output_images.reshape(
+                    (output_images.shape[0] * output_images.shape[1],) + output_images.shape[-3:]
+                )
+            )
+
+            image_logs.append(
+                {
+                    "validation_image": validation_image,
+                    "images": reshaped_output_pil_images,
+                    "validation_prompt": validation_prompt,
+                }
+            )
+
+        wandb_log_validation(image_logs)
+
+    return lambda validation_prompts, validation_images: __validate_lambda(validation_prompts, validation_images)
